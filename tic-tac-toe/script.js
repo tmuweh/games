050d79552de2d52a64751035a83bdecc50ff74fd
Tangue Muweh <atmuweh@gmail.com>
2018-08-05 13:08:50 +0200

    click event listener in all 9 cells

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -16,3 +16,19 @@ function clearBoard() {
 }
 
 restart.addEventListener('click', clearBoard);
+
+function changeMaker() {
+	if (this.textContent === '') {
+		this.textContent = 'X';
+	}
+	else if (this.textContent ==='X') {
+		this.textContent = 'O';
+	}
+	else{
+		this.textContent = ''
+	}
+}
+
+for(var i = 0; i<cells.length; i++){
+	cells[i].addEventListener('click', changeMaker)
+}
